67b0fa74cb5ebf49af198400c8150c1ed3257440
AbhaySindhikar <83824951+AbhaySindhikar@users.noreply.github.com>
2023-07-20 11:33:24 +0000

    Conversion of Data types

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const acctId = 14453
+let accountEmail = "nakul@gmail.com"
+var accountpaswd = "12345"
+accountCity = "Jaipur"
+// acctId=2
+console.log(acctId);
+accountEmail = "abhay@gmail.com"
+accountpaswd = "21221"
+accountCity = "Nagpur"
+
+console.table([accountEmail,acctId, accountpaswd,accountCity])
---
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -0,0 +1,11 @@
+let score="33"
+console.log(typeof score);
+
+let value= Number(score)
+console.log(typeof value);
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
