cb64092073fd26d648f1a0551720c01f2e9d08bd
AbhaySindhikar <83824951+AbhaySindhikar@users.noreply.github.com>
2023-07-23 09:56:50 +0000

    Conversion

--- .devcontainer/01_basics/03_conversionOperations.js
+++ .devcontainer/01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score="33"
+/*let score="33"
 console.log(typeof score);
 
 let value= Number(score)
@@ -9,3 +9,16 @@ let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 console.log(booleanIsLoggedIn);
+*/
+//**********************Operations ******************************
+
+let value=3
+let negvalue= -value
+console.log(negvalue);
+
+let str1="hello"
+let str2=" Nakul"
+str3=str1+str2
+console.log(str3);
+console.log("1"+2+2);
+console.log(2+2+"1");
